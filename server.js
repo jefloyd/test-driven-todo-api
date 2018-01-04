55577002ae0dd8b812c7ac16e5cc38d174624284
John Edwin Floyd <johnedwinfloyd@Johns-MacBook.local>
2018-01-03 17:52:55 -0700

    homework due

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // require express and other modules
-var express = require('express'),
+let express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
 
-// configure bodyParser (for receiving form and JSON data)
+// configure bodyParser (for receiving form data)
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
@@ -15,10 +14,10 @@ app.use(express.static(__dirname + '/public'));
  ************/
 
 // our database is an array for now with some hardcoded values
-var todos = [
-  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+let todos = [
+  { _id: 7, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 27, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 44, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -36,49 +35,90 @@ app.get('/', function homepage(req, res) {
 
 /*
  * JSON API Endpoints
- *
- * The comments below give you an idea of the expected functionality
- * that you need to build. These are basic descriptions, for more
- * specifications, see the todosTest.js file and the outputs of running
- * the tests to see the exact details. BUILD THE FUNCTIONALITY IN THE
- * ORDER THAT THE TESTS DICTATE.
  */
 
 app.get('/api/todos/search', function search(req, res) {
-  /* This endpoint responds with the search results from the
-   * query in the request. COMPLETE THIS ENDPOINT LAST.
-   */
+  let searchTerm = req.query.q;
+  console.log(searchTerm);
+  let filteredTodos = todos.filter(function(todo){
+    return(todo.task.toLowerCase().includes(searchTerm.toLowerCase()) || todo.description.toLowerCase().includes(searchTerm.toLowerCase()));
+  });
+  res.json({data : filteredTodos});
 });
 
+// get all todos
 app.get('/api/todos', function index(req, res) {
-  /* This endpoint responds with all of the todos
-   */
+  // send all todos as JSON response
+  res.json({ data: todos });
 });
 
+// create new todo
 app.post('/api/todos', function create(req, res) {
-  /* This endpoint will add a todo to our "database"
-   * and respond with the newly created todo.
-   */
+  // create new todo with form data (`req.body`)
+  let newTodo = req.body;
+
+  // set sequential id (last id in `todos` array + 1)
+  if (todos.length > 0) {
+    newTodo._id = todos[todos.length - 1]._id + 1;
+  } else {
+    newTodo._id = 1;
+  }
+
+  // add newTodo to `todos` array
+  todos.push(newTodo);
+
+  // send newTodo as JSON response
+  res.json(newTodo);
 });
 
+// get one todo
 app.get('/api/todos/:id', function show(req, res) {
-  /* This endpoint will return a single todo with the
-   * id specified in the route parameter (:id)
-   */
+  // get todo id from url params (`req.params`)
+  let todoId = parseInt(req.params.id);
+
+  // find todo to by its id
+  let foundTodo = todos.filter(function (todo) {
+    return todo._id == todoId;
+  })[0];
+
+  // send foundTodo as JSON response
+  res.json(foundTodo);
 });
 
+// update todo
 app.put('/api/todos/:id', function update(req, res) {
-  /* This endpoint will update a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with the newly updated todo.
-   */
+  // get todo id from url params (`req.params`)
+  let todoId = parseInt(req.params.id);
+
+  // find todo to update by its id
+  let todoToUpdate = todos.filter(function (todo) {
+    return todo._id === todoId;
+  })[0];
+
+  // update the todo's task
+  todoToUpdate.task = req.body.task;
+
+  // update the todo's description
+  todoToUpdate.description = req.body.description;
+
+  res.json(todoToUpdate);
 });
 
+// delete todo
 app.delete('/api/todos/:id', function destroy(req, res) {
-  /* This endpoint will delete a single todo with the
-   * id specified in the route parameter (:id) and respond
-   * with deleted todo.
-   */
+  // get todo id from url params (`req.params`)
+  let todoId = parseInt(req.params.id);
+
+  // find todo to delete by its id
+  let todoToDelete = todos.filter(function (todo) {
+    return todo._id === todoId;
+  })[0];
+
+  // remove todo from `todos` array
+  todos.splice(todos.indexOf(todoToDelete), 1);
+
+  // send back deleted todo
+  res.json(todoToDelete);
 });
 
 /**********
